189c8185c0ca7ef0613dd6fd6692b725409f0351
Rudy Ajuchán <rudyajuchansec32@gmail.com>
2023-09-29 17:29:09 -0600

    avanzando notificaciones

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'pages/Login/splash_view.dart';
 import 'package:get/get_navigation/get_navigation.dart';
+import 'package:asistencia_ceg/api/firebase_api.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  await FirebaseApi().initNotifications();
   runApp(const MyApp());
 }
 
